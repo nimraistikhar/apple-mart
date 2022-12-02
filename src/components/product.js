608e0e91783d912bab22b4733c05ec23ec3ab77a
nimra <nimra@myyogateacher.com>
2022-12-03 00:12:47 +0500

    made basic files

--- src/components/product.js
+++ src/components/product.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function product() {
+const Product = () => {
     return (
-        <div>product</div>
+        <div>Product</div>
     )
 }
 
-export default product
+export default Product
