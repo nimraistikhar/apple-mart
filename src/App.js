a5db06cfc3126abdf1090d9786126fcd59e56913
nimra <nimra@myyogateacher.com>
2022-12-08 23:53:11 +0500

    made routes for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Route } from "react-router-dom";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Home from "./components/home";
+import Product from "./components/product";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route to="/" />
+        <Route to="/" component={Home} />
+        <Route to="/products" component={Product} />
+        <Route to="/about" component={About} />
+        <Route to="/contact" component={Contact} />
+        <Redirect to="/" />
       </Switch>
     </>
   );
