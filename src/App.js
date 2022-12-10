b7d4e1ab18d534c3a8265350bb91c8acaeec36c1
nimra <nimra@myyogateacher.com>
2022-12-10 19:13:39 +0500

    made Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Route } from "react-router-dom";
+import React from 'react'
+import { Redirect, Route, Switch } from "react-router-dom";
 import About from "./components/about";
 import Contact from "./components/contact";
+import Footer from './components/footer';
+import Header from './components/header';
 import Home from "./components/home";
 import Product from "./components/product";
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route to="/" component={Home} />
         <Route to="/products" component={Product} />
@@ -14,6 +18,7 @@ function App() {
         <Route to="/contact" component={Contact} />
         <Redirect to="/" />
       </Switch>
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
